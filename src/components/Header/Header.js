8d1372ffc159fd79545eebebc493cd0d3b6134ce
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2025-10-01 20:31:28 +0300

    Update broken resume link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import logo from '../assets/My Logo.jpg';
-import './Header.css';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../assets/My Logo.jpg";
+import "./Header.css";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -17,13 +17,28 @@ const Header = () => {
           <img src={logo} alt="Logo" className="logo" />
         </div>
         <nav className="nav-menu">
-          <a href="#home" className="nav-link">Home</a>
-          <a href="#about" className="nav-link">About</a>
-          <a href="#projects" className="nav-link">Projects</a>
-          <a href="#contact" className="nav-link">Contact</a>
+          <a href="#home" className="nav-link">
+            Home
+          </a>
+          <a href="#about" className="nav-link">
+            About
+          </a>
+          <a href="#projects" className="nav-link">
+            Projects
+          </a>
+          <a href="#contact" className="nav-link">
+            Contact
+          </a>
         </nav>
         <div className="CV">
-          <a href="https://docs.google.com/document/d/1izNcOQh23qZgB4AZ7qQ5v9FbVfG60OrYvbnRg8QtJKE/edit?usp=drive_link" target="_blank" rel="noreferrer" className="resume-link">Get My Resume</a>
+          <a
+            href="https://drive.google.com/file/d/1Ao7NYiHL_nPVen2CCEqe2ELrHt4hm402/view?usp=drive_link"
+            target="_blank"
+            rel="noreferrer"
+            className="resume-link"
+          >
+            Get My Resume
+          </a>
         </div>
       </div>
 
@@ -32,10 +47,18 @@ const Header = () => {
       </button>
       {showMenu && (
         <nav className="nav-menu-mob">
-          <a href="#home" className="nav-link">Home</a>
-          <a href="#about" className="nav-link">About</a>
-          <a href="#projects" className="nav-link">Projects</a>
-          <a href="#contact" className="nav-link">Contact</a>
+          <a href="#home" className="nav-link">
+            Home
+          </a>
+          <a href="#about" className="nav-link">
+            About
+          </a>
+          <a href="#projects" className="nav-link">
+            Projects
+          </a>
+          <a href="#contact" className="nav-link">
+            Contact
+          </a>
         </nav>
       )}
     </header>
